26ba53e8c875d3927c3979180f3f61ba1d1c29c4
Andrea8802 <andreabonaventura802@gmail.com>
2022-12-27 19:08:28 +0100

    Creato componente header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,57 @@ export default {
 </script>
 
 <template>
-    
+    <header>
+        <img src="src/assets/img/logo.svg" alt="Logo" id="logo-l">
+        <img src="src/assets/img/logo-small.svg" alt="Logo" class="logo-s">
+        <a href="#" class="upgrade">
+            EFFETTUA L'UPGRADE
+        </a>
+    </header>
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
 
+header {
+    height: 90px;
+    position: fixed;
+    top: 0px;
+    right: 0px;
+    left: 0px;
+    z-index: 500;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 0 15px;
+    @include d-flex(space-between, center);
+    overflow: hidden;
+
+    #logo-l {
+        height: 45%;
+    }
+
+    .logo-s {
+        display: none;
+        filter: invert(0);
+        margin-left: 4px;
+        height: 55%;
+    }
+
+    .upgrade {
+        border: 2px solid white;
+        border-radius: 30px;
+        color: white;
+        font-weight: 600;
+        padding: 15px 40px;
+        font-size: 12px;
+        transition: all 0.3s ease;
+
+        &:hover {
+            font-size: 14px;
+            font-weight: bold;
+            margin-right: -10px;
+            background-color: white;
+            color: black;
+        }
+    }
+}
 </style>
