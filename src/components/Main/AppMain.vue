7513cc074f1d4594dfbf4037bfe42af727b0b4a5
Andrea8802 <andreabonaventura802@gmail.com>
2022-12-28 11:54:12 +0100

    Creato left menu

---
+++ src/components/Main/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+import LeftMenu from './LeftMenu.vue';
+
+export default {
+    name: "AppMain",
+    components: {
+        LeftMenu
+    }
+}
+</script>
+
+<template>
+    <main>
+        <LeftMenu />
+    </main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Main/LeftMenu.vue
@@ -0,0 +1,284 @@
+<script>
+export default {
+    name: "LeftMenu"
+}
+</script>
+
+<template>
+    <nav class="left-menu">
+        <div class="top">
+            <ul class="pages">
+                <li>
+                    <!-- Da nascondere -->
+                    <img src="src/assets/img/logo-small.svg" alt="Small Logo" class="logo-s">
+                </li>
+
+                <li id="selected">
+                    <a href="#">
+                        <img src="src/assets/img/home.svg" alt="Home">
+                        <span>
+                            Home
+                        </span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <img src="src/assets/img/search.svg" alt="Search">
+                        <span>
+                            Cerca
+                        </span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <img src="src/assets/img/libreria.svg" alt="">
+                        <span>
+                            La Tua Libreria
+                        </span>
+                    </a>
+                </li>
+            </ul>
+
+            <div class="create">
+                <h6>
+                    PLAYLIST
+                </h6>
+                <a href="#">
+                    <span class="icon">
+                        <fa icon="fa-solid fa-square-plus" />
+                    </span>
+                    Crea Playlist
+                </a>
+            </div>
+        </div>
+
+        <div class="middle">
+            <div class="playlist">
+                <ul>
+                    <li>
+                        <a href="#">
+                            Nome Playlist
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Nome Playlist
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Nome Playlist
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Nome Playlist
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Nome Playlist
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Nome Playlist
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Nome Playlist
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="bottom">
+            <a href="#" class="download">
+                <img src="src/assets/img/download.svg" alt="">
+                <span>
+                    Installa app
+                </span>
+            </a>
+            <hr>
+            <a href="#" class="profile">
+                <img src="src/assets/img/profile.svg" alt="">
+                <span>
+                    Jonh Doe
+                </span>
+            </a>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+@use '../../styles/partials/mixins' as *;
+
+.left-menu {
+    width: 230px;
+    height: calc(100vh - 90px);
+    z-index: 499;
+    padding: 90px 10px 0;
+    background-color: black;
+    color: white;
+    @include d-flex(flex-start, flex-start);
+    flex-direction: column;
+
+    img {
+        vertical-align: middle;
+        filter: invert(0.5);
+    }
+
+    .pages {
+
+        img {
+            width: 30px;
+            height: 30px;
+            padding-right: 2px;
+        }
+
+        li {
+            margin: 8px 0;
+            cursor: pointer;
+
+            &:hover a {
+                color: white;
+                transition: all 0.3s ease;
+            }
+
+            &:hover img {
+                filter: invert(0);
+                transition: all 0.3s ease;
+            }
+        }
+
+        a {
+            font-weight: bold;
+            font-size: 13px;
+            color: gray;
+            padding-left: 5px;
+            font-weight: 600;
+        }
+    }
+
+    .logo-s {
+        filter: invert(0);
+    }
+
+    #selected {
+        border-left: 3px solid #adff2f;
+        margin-left: -10px;
+        padding-left: 6px;
+
+        img {
+            filter: invert(0);
+        }
+
+        a {
+            color: white;
+        }
+    }
+
+    .create {
+        h6 {
+            font-size: 14px;
+            font-weight: bold;
+            margin: 20px 0 10px
+        }
+
+        a {
+            font-size: 15px;
+            font-weight: bold;
+            color: grey;
+            transition: all 0.3s ease;
+        }
+
+        .icon {
+            font-size: 44px;
+            vertical-align: middle;
+            margin-right: 5px;
+        }
+
+        &:hover a {
+            color: white;
+        }
+    }
+
+    .middle {
+        overflow-y: scroll;
+        height: 100%;
+        width: 100%;
+
+        .playlist {
+            padding-left: 5px;
+
+            li {
+                margin-top: 15px;
+
+                &:hover a {
+                    color: white;
+                }
+
+                a {
+                    color: gray;
+                    font-weight: bold;
+                    font-size: 13px;
+                    transition: all 0.2s ease;
+                }
+
+            }
+        }
+
+    }
+
+    .bottom {
+        margin: 10px 0;
+
+        hr {
+            margin: 8px 0;
+        }
+
+        .download {
+            font-size: 12px;
+            color: grey;
+            font-weight: bold;
+
+            &:hover span {
+                color: white;
+            }
+
+            &:hover img {
+                filter: invert(0);
+            }
+
+            * {
+                transition: all 0.3s ease;
+            }
+
+            img {
+                width: 20px;
+                height: 20px;
+                padding-right: 2px;
+            }
+        }
+    }
+
+    .profile {
+        font-size: 14px;
+        font-weight: bold;
+        color: white;
+
+        img {
+            width: 25px;
+            height: 25px;
+            background-color: gray;
+            filter: invert(0);
+            border-radius: 50%;
+            margin-right: 8px;
+        }
+    }
+}
+</style>
