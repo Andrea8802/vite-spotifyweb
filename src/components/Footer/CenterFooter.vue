d8d71bc1cec118314bed1716d6bd2f82a390a030
Andrea8802 <andreabonaventura802@gmail.com>
2023-01-08 12:57:18 +0100

    Dinamicizzata la barra temporale e aggiunto tasto reset

--- src/components/Footer/CenterFooter.vue
+++ src/components/Footer/CenterFooter.vue
@@ -11,17 +11,21 @@ export default {
         clickPause() {
 
             // Gestione tasto play/pausa
-            if (store.trackPreview.trackClicked) {
-                store.trackPreview.trackClicked = false;
+            if (store.trackStarted) {
+                store.trackStarted = false;
                 store.audioTraccia.pause();
                 clearInterval(store.playPausa);
 
             } else {
-                store.trackPreview.trackClicked = true;
+                store.trackStarted = true;
                 store.audioTraccia.play();
                 store.playPausa = setInterval(store.conteggioTempo, 1000);
 
             }
+        },
+
+        reset() {
+            store.resetTrack = true;
         }
     }
 
@@ -35,11 +39,10 @@ export default {
         <div class="icon">
             <fa icon="fa-solid fa-shuffle" />
             <fa icon="fa-solid fa-backward-step" />
-            <fa icon="fa-regular fa-circle-pause" class="play" v-if="store.trackPreview.trackClicked"
-                @click="clickPause" />
+            <fa icon="fa-regular fa-circle-pause" class="play" v-if="store.trackStarted" @click="clickPause" />
             <fa icon="fa-regular fa-circle-play" class="play" v-else @click="clickPause" />
             <fa icon="fa-solid fa-forward-step" />
-            <fa icon="fa-solid fa-rotate-right" />
+            <fa icon="fa-solid fa-rotate-right" @click="reset" />
         </div>
         <br>
 
@@ -58,7 +61,7 @@ export default {
                 </div>
             </div>
             <span>
-                4:35
+                1:30
             </span>
         </div>
     </div>
@@ -112,7 +115,7 @@ export default {
             width: 60%;
 
             .light-bar {
-                width: 20%;
+                width: 0%;
             }
 
 
--- src/store.js
+++ src/store.js
@@ -6,8 +6,8 @@ export const store = reactive({
     artistiTrovati: [],
 
     // Ricerca Utente
-    ricercaUtente: "",  // v-model per input text
-    elementoCercato: "",    // variabile per salvare la ricerca
+    ricercaUtente: "", // v-model per input text
+    elementoCercato: "", // variabile per salvare la ricerca
     ricercaEffettuata: false, // Flag ricerca finita
     ricercaInCorso: false, // Flag ricerca in corso
 
@@ -20,15 +20,34 @@ export const store = reactive({
         title: "",
         artist: "",
         img: "",
-        trackClicked: false // Gestione tasto play
     },
 
-    audioTraccia: "",
-    timeTrack: "00:00",
-    secondsTrack: 0,
-    minuteTrack: 0,
+
+    playPausa: "", // Gestione start o stop del setInterval della traccia musicale
+    audioTraccia: "", // Variabile che contiene l'audio della traccia
+
+    timeTrack: "00:00", // Variabile per mostrare il tempo trascorso con la traccia
+    minuteTrack: 0, // Variabile per gestire i minuti
+    secondsTrack: 0, // Variabile per gestire i secondi
+
+    barSize: 0, // Variabile per gestire la barra temporale
+    resetTrack: false, // Flag per gestire il reset della traccia
+    trackStarted: false, // Gestione tasto play
 
     conteggioTempo: (() => {
+        let bar = document.querySelector(".foot.center .light-bar");
+
+        // Se la traccia arriva alla fine o se viene resettata
+        if (store.secondsTrack === 30 && store.minuteTrack === 1 || store.resetTrack) {
+            clearInterval(store.playPausa);
+            store.audioTraccia.pause();
+            store.secondsTrack, store.minuteTrack, store.barSize = 0;
+            store.timeTrack = "00:00";
+            bar.style.transition = "all 0s linear"
+            bar.style.width = store.barSize + "%";
+            store.trackStarted = false;
+            return;
+        }
         if (store.secondsTrack === 59) {
             store.minuteTrack++;
             store.secondsTrack = 0;
@@ -43,9 +62,14 @@ export const store = reactive({
         } else {
             store.timeTrack = `0${store.minuteTrack}:0${store.secondsTrack}`
         }
+
+        // Gestione barra temporale traccia
+        store.barSize += 1.11;
+        bar.style.width = store.barSize + "%";
+        bar.style.transition = "all 1s linear"
+
     }),
 
-    playPausa: "",
 
     // Links del left menù
     linksLeftMenu: [
